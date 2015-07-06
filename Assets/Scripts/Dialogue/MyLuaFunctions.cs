babcd2813915f44f9cc10eca4501a84e1f345f72
Pieter Parker <pieterjparker@gmail.com>
2015-07-06 17:52:34 -0600

    Added new user signup

--- Assets/Scripts/Dialogue/MyLuaFunctions.cs
+++ Assets/Scripts/Dialogue/MyLuaFunctions.cs
@@ -23,12 +23,16 @@ public class MyLuaFunctions : MonoBehaviour {
 		Lua.RegisterFunction("GameObjectExists", this, typeof(MyLuaFunctions).GetMethod("GameObjectExists"));
 		Lua.RegisterFunction("InputIsValid", this, typeof(MyLuaFunctions).GetMethod("InputIsValid"));
 		Lua.RegisterFunction("InputIsEmail", this, typeof(MyLuaFunctions).GetMethod("InputIsEmail"));
+		Lua.RegisterFunction("PasscodeIsValid", this, typeof(MyLuaFunctions).GetMethod("PasscodeIsValid"));
+		Lua.RegisterFunction("PasscodesMatch", this, typeof(MyLuaFunctions).GetMethod("PasscodesMatch"));
 	}
 	
 	void OnDisable() {
 		Lua.UnregisterFunction("GameObjectExists");
 		Lua.UnregisterFunction("InputIsValid");
 		Lua.UnregisterFunction("InputIsEmail");
+		Lua.UnregisterFunction("PasscodeIsValid");
+		Lua.UnregisterFunction("PasscodesMatch");
 	}
 	
 	public bool GameObjectExists(string name) {
@@ -64,5 +68,32 @@ public class MyLuaFunctions : MonoBehaviour {
 			return false;
 		}
 	}
+
+	public bool PasscodesMatch(string passcode1, string passcode2) {
+		string passcode1result = DialogueLua.GetVariable (passcode1).AsString;
+		string passcode2result = DialogueLua.GetVariable (passcode2).AsString;
+		Debug.Log (passcode1result);
+		Debug.Log (passcode2result);
+		if (passcode1result == passcode2result) {
+			Debug.Log ("Passcodes Match!");
+			return true;
+		} else {
+			Debug.Log ("Passcodes Don't Match!");
+			return false;
+		}
+	}
+
+	public bool PasscodeIsValid(string passcode) {
+		string result = DialogueLua.GetVariable (passcode).AsString;
+		Debug.Log (result);
+		if (result.Length > 5) {
+			Debug.Log ("Passcodes Is Valid!");
+			return true;
+		} else {
+			Debug.Log ("Passcodes Isnt Valid!");
+			return false;
+		}
+	}
+
 
 }
--- Assets/Scripts/Dialogue/SequenceCommands/CreateUser.cs
+++ Assets/Scripts/Dialogue/SequenceCommands/CreateUser.cs
@@ -5,9 +5,12 @@ using Parse;
 using System.Threading.Tasks;
 
 namespace PixelCrushers.DialogueSystem.SequencerCommands {
-
+	
 	public class SequencerCommandCreateUser : SequencerCommand {
 
+		private enum State { Idle, Loading, Success, Error }
+		private State state = State.Idle;
+		
 		public void Start() {
 			// Add your initialization code here. You can use the GetParameter***() and GetSubject()
 			// functions to get information from the command's parameters. You can also use the
@@ -17,29 +20,73 @@ namespace PixelCrushers.DialogueSystem.SequencerCommands {
 			//
 			// If your sequencer command only does something immediately and then finishes,
 			// you can call Stop() here and remove the Update() method.
-			Debug.Log ("No user yet! Create one!");
+			//Disable Continue
+
+			DialogueLua.SetVariable ("DisableContinue", true);
+
+			string username = Lua.Run("return Variable['Signup.Username']").AsString;
+			string email = Lua.Run("return Variable['Signup.Email']").AsString;
+			string password = Lua.Run("return Variable['Signup.Password']").AsString;
+
+			Debug.Log ("CreateUser");
+			Debug.Log (username);
+			Debug.Log (email);
+			Debug.Log (password);
+
+			if (username == null) return;
+			if (email == null) return;
+			if (password == null) return;
+
+			state = State.Loading;
 			var user = new ParseUser()
 			{
-			    Username = "Pieter",
-			    Password = "Password",
-			    Email = "pieterjparker@gmail.com"
+				Username = username,
+				Email = email,
+				Password = password
 			};
 
-			Task signUpTask = user.SignUpAsync();
-			Stop ();
+			user.SignUpAsync().ContinueWith(t => {
+				if (t.IsFaulted) {
+					Debug.Log ("User Signup Error!"); 
+					state = State.Error;
+				} else {
+					Debug.Log ("User Signup Succeeded!");
+					state = State.Success;
+				}
+			});
 		}
-
+		
 		public void Update() {
 			// Add your update code here. When the command is done, call Stop().
 			// If you've called stop above in Start(), you can delete this method.
+			//Debug.Log (queryCompleted);
+			switch (state) {
+			case State.Idle:
+				Debug.Log("Idle!");
+				break;
+			case State.Loading:
+				Debug.Log("Loading!");
+				break;
+			case State.Success:
+				Debug.Log("Success!");
+				DialogueLua.SetVariable ("Signup.UserFound",false);
+				Stop ();
+				break;
+			case State.Error:
+				Debug.Log("Error");
+				DialogueLua.SetVariable ("Signup.UserFound",true);
+				Stop ();
+				break;
+			}
 		}
-
+		
 		public void OnDestroy() {
 			// Add your finalization code here. This is critical. If the sequence is cancelled and this
 			// command is marked as "required", then only Start() and OnDestroy() will be called.
 			// If you don't need to do anything at the end, you can delete this method.
+			DialogueLua.SetVariable ("DisableContinue", false);
 		}
-
+		
 	}
-
+	
 }
